235c421281f23cf8cb014d8df0010ee3fd940edb
U-PATRICIA\Patou <Patou@patricia.(none)>
2010-08-14 16:41:28 +0200

    Fin de migration windows

--- package-production.sh
+++ package-production.sh
@@ -1,1 +1,1 @@
-mvn clean package -Dglassfish-home=/home/jsmadja/code/glassfish-3.0.1/glassfish -Denvironment-name=production -DskipTests=true
+mvn clean package -Dglassfish-home="C:\Program Files\glassfish-3.0.1\glassfish" -Denvironment-name=production -DskipTests=true
--- test-development.sh
+++ test-development.sh
@@ -1,1 +1,1 @@
-mvn clean test -Dglassfish-home=/home/jsmadja/code/glassfish-3.0.1/glassfish -Denvironment-name=development
+mvn clean test -Dglassfish-home="C:\Program Files\glassfish-3.0.1\glassfish" -Denvironment-name=development
