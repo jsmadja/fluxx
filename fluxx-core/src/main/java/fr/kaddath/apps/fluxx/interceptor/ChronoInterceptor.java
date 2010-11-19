6c7fb3080fe0648ac46baa0b321903ab49f68390
jsmadja <jsmadja@xebia.fr>
2010-11-19 17:42:50 +0100

    optimisation

--- fluxx-core/src/main/java/fr/kaddath/apps/fluxx/interceptor/ChronoInterceptor.java
+++ fluxx-core/src/main/java/fr/kaddath/apps/fluxx/interceptor/ChronoInterceptor.java
@@ -8,14 +8,15 @@ import javax.interceptor.InvocationContext;
 
 public class ChronoInterceptor {
 
-    private static final Logger LOG = Logger.getLogger(ChronoInterceptor.class.getName());
+	private static final Logger LOG = Logger.getLogger(ChronoInterceptor.class.getName());
 
-    @AroundInvoke
-    public Object intercept(InvocationContext ctx) throws java.lang.Exception {
-        long timeMillis = System.currentTimeMillis();
-        Object o = ctx.proceed();
-        long currentTimeMillis = System.currentTimeMillis();
-        LOG.log(Level.INFO, "{0} executed in {1} ms", new Object[]{ctx.getClass().getName()+"."+ctx.getMethod().getName(), currentTimeMillis - timeMillis});
-        return o;
-    }
+	@AroundInvoke
+	public Object intercept(InvocationContext ctx) throws java.lang.Exception {
+		long timeMillis = System.currentTimeMillis();
+		Object o = ctx.proceed();
+		long currentTimeMillis = System.currentTimeMillis();
+		String methodName = ctx.getClass().getName() + "." + ctx.getMethod().getName();
+		LOG.log(Level.INFO, "{0} executed in {1} ms", new Object[] { methodName, currentTimeMillis - timeMillis });
+		return o;
+	}
 }
